fca93beea8966feda2312c94c7c81f5b24b0fbae
GillinedUp <myrreah@gmail.com>
2016-11-27 00:15:33 +0100

    updated tests

--- src/agh/cs/lab2/CarSystemTest.java
+++ src/agh/cs/lab2/CarSystemTest.java
@@ -3,6 +3,10 @@ package agh.cs.lab2;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by yurii on 11/3/16.
  */
@@ -18,4 +22,14 @@ public class CarSystemTest {
         assertEquals(oldPosition, car1.getPosition());
     }
 
+    @Test
+    public void testCarUnboundedMap(){
+        List<IMapElement> elementList = new LinkedList<>();
+        IWorldMap map = new UnboundedMap(elementList);
+        Car car1 = new Car(map);
+        map.add(car1);
+        Position oldPosition = car1.getPosition();
+        map.run(new OptionsParser().parse(new String[]{"right", "f", "forward", "f", "l", "f", "left", "f", "forward", "f", "r", "b"}));
+        assertEquals(oldPosition, car1.getPosition());
+    }
 }
